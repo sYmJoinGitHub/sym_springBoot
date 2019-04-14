d2dbb1199d2c9b72e753a5b11fbc254e65dda2e5
sym <sym542569199@gmail.com>
2019-04-14 12:47:53 +0800

    简易搭建ELK平台

---
+++ springBoot-ELK/src/main/java/com/sym/LogApplication.java
@@ -0,0 +1,15 @@
+package com.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: shenym
+ * @Date: 2019-03-08 9:47
+ */
+@SpringBootApplication
+public class LogApplication {
+    public static void main(String[] args){
+        SpringApplication.run(LogApplication.class,args);
+    }
+}

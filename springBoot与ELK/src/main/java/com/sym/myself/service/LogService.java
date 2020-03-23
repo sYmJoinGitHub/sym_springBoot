21b829675dfda6b75290ccbdba8651769591ae8f
shenyanming <sym542569199@gmail.com>
2020-03-23 19:55:00 +0800

    springBoot日志ELK

--- springBoot与ELK/src/main/java/com/sym/myself/service/LogService.java
+++ springBoot与ELK/src/main/java/com/sym/myself/service/LogService.java
@@ -1,5 +1,7 @@
 package com.sym.myself.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +10,19 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LogService {
+
+    // 定义日志记录器
+    private static Logger LOGGER = LoggerFactory.getLogger(LogService.class);
+
+    /**
+     * 打印各种日志
+     */
+    public void printLog(){
+        // 记录不同级别的日志
+        LOGGER.trace("trace级别...");
+        LOGGER.debug("debug级别...");
+        LOGGER.info("info级别...");
+        LOGGER.warn("wrn级别...");
+        LOGGER.error("error级别...");
+    }
 }
--- springBoot与ELK/src/test/java/com/sym/MainTest.java
+++ springBoot与ELK/src/test/java/com/sym/MainTest.java
@@ -1,9 +1,9 @@
 package com.sym;
 
+import com.sym.myself.service.LogService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,15 +15,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class MainTest {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(MainTest.class);
+    @Autowired
+    private LogService logService;
 
     @Test
-    public void testOne(){
-        LOGGER.debug("搭建ELK平台...");
-        LOGGER.info("君不见黄河之水天上来");
-        LOGGER.warn("奔流到海不复回");
-        LOGGER.error("君不见高堂明镜悲白发");
-        LOGGER.trace("朝如青丝暮成雪");
+    public void test() throws Exception {
+        logService.printLog();
     }
 
 }
